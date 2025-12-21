68c04916796d1a885c718d1817ea38b73b9cc506
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-12-21 14:36:58 +0330

    fix(removed unused fonts and optimised image's size)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,34 +3,31 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
-      container:{
-        center:true,
-        padding:{
-          DEFAULT:"0.7rem",
-          xl:"15px",
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "0.7rem",
+          xl: "15px",
         },
-        
       },
-      backgroundImage:{
-        "banner":"url(.././images/1.png)",
-        "banner2":"url(.././images/download.jpg)",
+      backgroundImage: {
+        banner: "url(.././images/1.webp)",
+        banner2: "url(.././images/download.webp)",
       },
-      fontFamily:{
-        "dana-sm":"dana-sm",
-        "dana-md":"dana-md",
-        "dana-bold":"dana-bold",
-        "py-sm":"py-sm",
-        "py-md":"py-md",
-        "py-bold":"py-bold",
+      fontFamily: {
+        "dana-sm": "dana-sm",
+        "dana-md": "dana-md",
+        "py-md": "py-md",
+        "py-bold": "py-bold",
       },
-      colors:{
-        "blue":"#028fcc",
-        "miniSky":"#b3ddf0",
+      colors: {
+        blue: "#028fcc",
+        miniSky: "#b3ddf0",
       },
-      screens:{
-        "1212px":"1212px",
-        "340px":"340px",
-      }
+      screens: {
+        "1212px": "1212px",
+        "340px": "340px",
+      },
     },
   },
   plugins: [],
